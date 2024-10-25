97f37f8dde57895b044cc0ed89182a88222e2ef8
rummansaaqeb <rsaaqeb@gmail.com>
2024-10-25 19:15:40 +0600

    finished rest countries using ReactJS

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,9 +1,13 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Country from "../Country/Country";
+import './Countries.css'
 
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
+    const [visitedCountries, setVisitedCountries] = useState([]);
+    const [visitedFlags, setVisitedFlags] = useState([]);
 
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
@@ -11,9 +15,47 @@ const Countries = () => {
         .then(data => setCountries(data))
     }, [])
 
+
+    const handleVisitedCountry = (country) => {
+        console.log('Add this to your visited country');
+        const newVisitedCountries = [...visitedCountries, country]
+        setVisitedCountries(newVisitedCountries);
+    }
+
+    const handleVisitedFlags = flag => {
+        const newVisitedFlags = [...visitedFlags, flag];
+        setVisitedFlags(newVisitedFlags);
+    }
+
+    // remove item from an array in a state
+    // use filter to select all the elements except the one you want to remove
+
+    
+    
+
     return (
         <div>
             <h3>Countries: {countries.length}</h3>
+            {/* visited country */}
+            <div>
+            <h5>Visited Countries: {visitedCountries.length}</h5>
+            <ul>
+                {
+                    visitedCountries.map(country => <li key={country.cca3}>{country.name.common}</li>)
+                }
+            </ul>
+            </div>
+            <div className="flag-container">
+                {
+                    visitedFlags.map((flag, idx) => <img key={idx} src={flag}></img>)
+                }
+            </div>
+            {/* display countries */}
+            <div className="country-container">
+            {
+                countries.map(country => <Country key={country.cca3} country={country} handleVisitedCountry={handleVisitedCountry} handleVisitedFlags={handleVisitedFlags}></Country>)
+            }
+            </div>
         </div>
     );
 };
